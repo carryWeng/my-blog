f74a78652c7bb2a68eada39e39eda49d81938abd
carryWeng <carry_weng@163.com>
2023-06-25 16:21:49 +0800

    自动生成目录

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,58 @@
 import { defineConfig } from 'vitepress'
 
+const fs = require("fs");
+const path = require("path");
+
+const docsPath = path.dirname(__dirname); // docs 目录路径
+const sidebarConfig = generateSidebarConfig(docsPath);
+
+function generateSidebarConfig(docsPath, link = '', index = 0) {
+  const sidebarConfig = {};
+  const files = fs.readdirSync(docsPath);
+
+  files.forEach((filename) => {
+    if (filename.startsWith(".")) return;
+    const filepath = path.join(docsPath, filename);
+    const stat = fs.statSync(filepath);
+    // 如果是文件夹，则递归生成子级 sidebar 配置
+    if (stat.isDirectory()) {
+      if (index === 0) {
+        const config = generateSidebarConfig(filepath, `/${filename}/`, index + 1);
+        if (!sidebarConfig[`/${filename}/`]) {
+          sidebarConfig[`/${filename}/`] = [config];
+        }
+      } else {
+        if (!sidebarConfig.items) {
+          sidebarConfig.items = [];
+        }
+        sidebarConfig.items.push(generateSidebarConfig(filepath, `${link}${filename}/`, index + 1))
+      }
+    } else {
+      const extname = path.extname(filepath);
+      const basename = path.basename(filepath, extname);
+      if (filename === 'index.md' && index > 0) {
+        const menuPath = path.dirname(filepath);
+        const menuName = path.basename(menuPath) 
+        sidebarConfig.text = menuName;
+        sidebarConfig.link = link;
+      }
+      if (extname === ".md" && filename !== "index.md") {
+        if (!sidebarConfig.items) {
+          sidebarConfig.items = [];
+        }
+        sidebarConfig.items.push({
+          text: basename,
+          link: `${link}${basename}`,
+        });
+      }
+    }
+  });
+
+  return sidebarConfig;
+}
+
+
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   lang: 'zh-CN',
@@ -23,28 +76,11 @@ export default defineConfig({
       next: '下一页',
     },
 
-    sidebar: [
-      {
-        text: '文档',
-        collapsed: true,
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-          { text: '面试题', link: '/面试题' },
-        ]
-      },
-      {
-        text: 'es6',
-        collapsed: true,
-        items: [
-          { text: '01.ECMAScript 6 简介', link: '/ES6 Tutorial notes/01.ECMAScript 6 简介' },
-        ]
-      },
-    ],
+    sidebar: sidebarConfig,
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ],
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    // ],
   },
   cleanUrls: true,
 })
